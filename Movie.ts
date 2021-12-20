3735346c37c9bdf34ff3a80e48cdecfbe756dfe1
huangchingting <qwe0918190417@gmail.com>
2021-12-20 16:28:46 +0800

    update <Movie.ts>

---
+++ Movie.ts
@@ -0,0 +1,14 @@
+class Movie
+{
+    title: string;
+    recommended: boolean;
+    publishedDate: Date;
+
+    constructor(title: string, recommended: boolean, publishedDate: Date)
+    {
+        this.title = title;
+        this.recommended = recommended;
+        this.publishedDate = publishedDate;
+    }
+
+}
